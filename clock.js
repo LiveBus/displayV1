0562ec24ec881a0ad3a3e87e1438979cbe6b670a
maxtkc <maxkatzchristy@gmail.com>
2018-08-15 23:08:45 -0400

    Added date, removed seconds

--- clock.js
+++ clock.js
@@ -1,29 +1,27 @@
 function showTime(){
     var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+    var hour = date.getHours(); // 0 - 23
+    var min = date.getMinutes(); // 0 - 59
+    var day = date.getDate(); // 0 - 31
+    var month = date.getMonth() + 1; // 1 - 12
     var session = "AM";
     
-    if(h == 0){
-        h = 12;
+    if(hour == 0){
+        hour = 12;
     }
     
-    if(h > 12){
-        h = h - 12;
+    if(hour > 12){
+        hour = hour - 12;
         session = "PM";
     }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
+
+    min = (min < 10) ? "0" + min : min;
+
+    var time = hour + ":" + min + " " + session + " " + month + "/" + day;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     
     setTimeout(showTime, 1000);
-    
 }
 
 showTime();
